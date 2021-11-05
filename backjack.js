5a9eaddf7a867b2ae73551c2ae788837dfc6189c
Manuel Herdez <herdez@gmail.com>
2021-11-05 09:59:51 -0600

    funciones pedirCarta y valorCarta - listas

--- backjack.js
+++ backjack.js
@@ -5,6 +5,10 @@ const buttons   = document.querySelectorAll('.btn');
 const imgCartas = document.querySelectorAll('img'); 
 const imgCartasJugador = document.querySelector('.cartasJugador');
 const imgCartasComputadora = document.querySelector('.cartasComputadora');
+const puntosJugador = document.querySelector('small');
+
+// VARIABLES
+let mazo = [];
 
 // EVENTOS
 buttons[0].addEventListener('click', () => {
@@ -13,9 +17,51 @@ buttons[0].addEventListener('click', () => {
 });
 
 buttons[1].addEventListener('click', () => {
-   imgCartasJugador.innerHTML += '<img src="./assets/cartas/QD.png">'
+   const carta = pedirCarta()
+   imgCartasJugador.innerHTML += `<img src="./assets/cartas/${carta}.png">`
+   valorCarta(carta)
 })
 
 buttons[2].addEventListener('click', () => {
-   imgCartasComputadora.innerHTML += '<img src="./assets/cartas/KC.png">'
+   imgCartasComputadora.innerHTML += `<img src="./assets/cartas/${pedirCarta()}.png">`
 })
+
+// FUNCIONES
+const crearMazo = () => {
+   let especiales = ['A','J','Q','K'];
+   let palo       = ['H','S','D','C'];
+
+   for(let n=2; n <= 10; n++) {
+      for(let p of palo) {
+         mazo.push(n+p)
+      }
+   }
+   for(let e of especiales) {
+      for(let p of palo) {
+         mazo.push(e+p);
+      }
+   }
+   mazo = _.shuffle(mazo);
+   console.log(mazo)
+}
+
+const pedirCarta = () => {
+   return mazo.shift();
+}
+
+
+// SI VALOR ES UN NUMERO ENTONCES SE DEVUELVE LA DENOMINACION DEL NUMERO
+// SI ES UNA A EL VALOR ES 11
+// SI ES UNA J,Q,K EL VALOR ES 10
+const valorCarta = (carta) => {
+   let valor = carta.substring(0, carta.length - 1);
+   if( isNaN(valor) ) {
+      valor = (valor === 'A') ? 11 : 10; 
+   } else {
+      valor *= 1
+   }
+   console.log(valor)
+}
+
+// INICIO
+crearMazo();
