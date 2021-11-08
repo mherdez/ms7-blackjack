9e7963913edb35cbc4a931ac946705ada4dc4930
Manuel Herdez <herdez@gmail.com>
2021-11-08 13:01:35 -0600

    Cartas Jugador

--- backjack.js
+++ backjack.js
@@ -1,66 +1,102 @@
 
 
 // REFERNCIAS HTML
-const buttons   = document.querySelectorAll('.btn');
-const imgCartas = document.querySelectorAll('img'); 
+const btnNuevoJuego = document.querySelector('#btnNuevoJuego');
+const btnPedirCarta = document.querySelector('#btnPedirCarta');
+const btnDetener = document.querySelector('#btnDetener');
+const imgCartas = document.querySelectorAll('img');
 const imgCartasJugador = document.querySelector('.cartasJugador');
 const imgCartasComputadora = document.querySelector('.cartasComputadora');
-const puntosJugador = document.querySelector('small');
+const puntosJugador = document.querySelectorAll('small')[0];
+const puntosComputadora = document.querySelectorAll('small')[1];
+const cartasJugador = document.querySelector('.cartasJugador');
 
 // VARIABLES
 let mazo = [];
+let totalPuntosJugador = 0;
+let totalPuntosComputadora = 0;
 
 // EVENTOS
-buttons[0].addEventListener('click', () => {
+btnNuevoJuego.addEventListener('click', () => {
+   console.clear();
+   mazo = [];
+   crearMazo();
    imgCartasJugador.innerHTML = '';
    imgCartasComputadora.innerHTML = '';
+   totalPuntosJugador = 0;
+   puntosJugador.textContent = totalPuntosJugador;
+   totalPuntosComputadora = 0;
+   puntosComputadora.textContent = totalPuntosComputadora;
+   btnPedirCarta.removeAttribute('disabled');
+   btnPedirCarta.classList.remove('btn-disabled');
+   btnPedirCarta.classList.add('bg-blue');
 });
 
-buttons[1].addEventListener('click', () => {
-   const carta = pedirCarta()
-   imgCartasJugador.innerHTML += `<img src="./assets/cartas/${carta}.png">`
-   valorCarta(carta)
+// OPTIMIZAR FUNCION (JUGADOR Y COMPUTADORA SON SIMILARES)
+// OPTIMIZAR LA CREACION DE LA CARTA
+btnPedirCarta.addEventListener('click', () => {
+   const carta = crearCarta();
+   totalPuntosJugador += valorCarta(carta);
+   puntosJugador.textContent = totalPuntosJugador;
+   if (totalPuntosJugador > 21) {
+      juegoPerdidoJugador();
+   }
 })
 
-buttons[2].addEventListener('click', () => {
-   imgCartasComputadora.innerHTML += `<img src="./assets/cartas/${pedirCarta()}.png">`
+btnDetener.addEventListener('click', () => {
+   const carta = pedirCarta();
+   imgCartasComputadora.innerHTML += `<img src="./assets/cartas/${carta}.png">`
+   totalPuntosComputadora += valorCarta(carta);
+   puntosComputadora.textContent = totalPuntosComputadora;
 })
 
 // FUNCIONES
 const crearMazo = () => {
-   let especiales = ['A','J','Q','K'];
-   let palo       = ['H','S','D','C'];
+   let especiales = ['A', 'J', 'Q', 'K'];
+   let palo = ['H', 'S', 'D', 'C'];
 
-   for(let n=2; n <= 10; n++) {
-      for(let p of palo) {
-         mazo.push(n+p)
+   for (let n = 2; n <= 10; n++) {
+      for (let p of palo) {
+         mazo.push(n + p)
       }
    }
-   for(let e of especiales) {
-      for(let p of palo) {
-         mazo.push(e+p);
+   for (let e of especiales) {
+      for (let p of palo) {
+         mazo.push(e + p);
       }
    }
    mazo = _.shuffle(mazo);
    console.log(mazo)
 }
 
+const crearCarta = () => {
+   const carta = document.createElement('img');
+   const valor = pedirCarta();
+   const src   = `./assets/cartas/${valor}.png`;
+   carta.src = src;
+   cartasJugador.append(carta)
+   return valor;
+}
+
 const pedirCarta = () => {
    return mazo.shift();
 }
 
-
-// SI VALOR ES UN NUMERO ENTONCES SE DEVUELVE LA DENOMINACION DEL NUMERO
-// SI ES UNA A EL VALOR ES 11
-// SI ES UNA J,Q,K EL VALOR ES 10
 const valorCarta = (carta) => {
    let valor = carta.substring(0, carta.length - 1);
-   if( isNaN(valor) ) {
-      valor = (valor === 'A') ? 11 : 10; 
+   if (isNaN(valor)) {
+      valor = (valor === 'A') ? 11 : 10;
    } else {
       valor *= 1
    }
-   console.log(valor)
+   return valor;
+}
+
+const juegoPerdidoJugador = () => {
+   console.log('el jugador ya perdio');
+   btnPedirCarta.setAttribute('disabled', true);
+   btnPedirCarta.classList.remove('bg-blue');
+   btnPedirCarta.classList.add('btn-disabled');
 }
 
 // INICIO
